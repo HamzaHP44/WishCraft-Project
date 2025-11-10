e58974e763937c9210947d215ca2b4f1ced612b4
HAMZA 23482 <hamza.g23482@iqra.edu.pk>
2025-11-10 07:00:10 +0000

    still inspire me remove this project and make new project and do that wh

--- src/ai/dev.ts
+++ src/ai/dev.ts
@@ -1,6 +1,4 @@
 import { config } from 'dotenv';
 config();
 
-import '@/ai/flows/personalize-quote-with-detail.ts';
-import '@/ai/flows/generate-inspirational-quote.ts';
 import '@/ai/flows/generate-birthday-wish.ts';
